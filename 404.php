7e60a73440bd4129c291d8ba3c4a2dbc99734b0a
Matthew McColm <matthewmccolm07@gmail.com>
2021-12-28 11:20:38 +0000

    Reverted Login Pages

--- 404.php
+++ 404.php
@@ -1,7 +1,3 @@
-<?php
-require('db.php');
-include("nonauth_session.php");
-?>
 
 <!DOCTYPE html>
 <html lang="en">
--- DofE.php
+++ DofE.php
@@ -1,7 +1,3 @@
-<?php
-require('db.php');
-include("auth_session.php");
-?>
 
 <!DOCTYPE html>
 <html lang="en">
